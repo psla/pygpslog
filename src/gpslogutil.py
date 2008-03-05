8d95ad0fd433aeca1793d851833b0f9ac19e75ff
johler <johler@ac97095f-ae47-0410-9732-21da4afb212a>
2008-03-05 12:01:06 +0000

    Initial import.

---
+++ src/gpslogutil.py
@@ -0,0 +1,343 @@
+import os, time, e32, e32dbm
+import settings
+from math import *
+from IndentedXMLWriter import XMLWriter
+
+FIXTYPES        = ["none","2D","3D","dgps"]
+
+METERS_TO_FEET	= 3.2808399
+DAYS_SINCE_1990	= 25569
+SECONDS_PER_DAY = 24*3600
+
+DEF_LOGDIR   = (os.path.isdir("E:\Data") and  "E:\Data\GpsLog") or "C:\Data\GpsLog"
+if e32.in_emulator(): DEF_LOGDIR = r"c:\python"
+
+def isoformat(ts):
+  return time.strftime("%Y-%m-%dT%H:%M:%SZ", time.gmtime(ts))
+
+def coord(c, sign="NS", dec=False):
+  if c == None:
+    return "---"
+  if not dec:
+    sg = sign[0]
+    if c < 0: sg = sign[1]; c = abs(c)
+    d = int(c); m = (c - d) * 60.0; s = (m - int(m)) * 60.0; m = int(m)
+    return u"%-2d\u00b0%02d'%-5.2f %s" % (d, m, s, sg)
+  else:
+    return u"%.6f" % c
+
+def sorted(itrbl):
+  ret =itrbl[:]
+  ret.sort()
+  return ret
+  
+
+def Deg2Rad(x):
+    "Degrees to radians."
+    return x * (pi/180)
+
+def CalcRad(lat):
+    "Radius of curvature in meters at specified latitude."
+    a = 6378.137
+    e2 = 0.081082 * 0.081082
+    # the radius of curvature of an ellipsoidal Earth in the plane of a
+    # meridian of latitude is given by
+    #
+    # R' = a * (1 - e^2) / (1 - e^2 * (sin(lat))^2)^(3/2)
+    #
+    # where a is the equatorial radius,
+    # b is the polar radius, and
+    # e is the eccentricity of the ellipsoid = sqrt(1 - b^2/a^2)
+    #
+    # a = 6378 km (3963 mi) Equatorial radius (surface to center distance)
+    # b = 6356.752 km (3950 mi) Polar radius (surface to center distance)
+    # e = 0.081082 Eccentricity
+    sc = sin(Deg2Rad(lat))
+    x = a * (1.0 - e2)
+    z = 1.0 - e2 * sc * sc
+    y = pow(z, 1.5)
+    r = x / y
+
+    r = r * 1000.0	# Convert to meters
+    return r
+
+def earthDistance((lat1, lon1), (lat2, lon2)):
+    "Distance in meters between two points specified in degrees."
+    x1 = CalcRad(lat1) * cos(Deg2Rad(lon1)) * sin(Deg2Rad(90-lat1))
+    x2 = CalcRad(lat2) * cos(Deg2Rad(lon2)) * sin(Deg2Rad(90-lat2))
+    y1 = CalcRad(lat1) * sin(Deg2Rad(lon1)) * sin(Deg2Rad(90-lat1))
+    y2 = CalcRad(lat2) * sin(Deg2Rad(lon2)) * sin(Deg2Rad(90-lat2))
+    z1 = CalcRad(lat1) * cos(Deg2Rad(90-lat1))
+    z2 = CalcRad(lat2) * cos(Deg2Rad(90-lat2))
+    a = (x1*x2 + y1*y2 + z1*z2)/pow(CalcRad((lat1+lat2)/2),2)
+    # a should be in [1, -1] but can sometimes fall outside it by
+    # a very small amount due to rounding errors in the preceding
+    # calculations (this is prone to happen when the argument points
+    # are very close together).  Thus we constrain it here.
+    if abs(a) > 1: a = 1
+    elif a < -1: a = -1
+    return CalcRad((lat1+lat2) / 2) * acos(a)
+
+    
+def simpleDistance(p1, p2):
+  lat1, lon1 = p1
+  lat2, lon2 = p2
+  try:
+    R = 6367000.0 # meters
+    dlon = math.radians(abs(lon1 - lon2))
+    dlat = math.radians(abs(lat1 - lat2))
+    slat = math.radians(lat1)
+    olat = math.radians(lat2)
+    a = math.sin(dlat/2.0) ** 2.0 + math.cos(slat) * math.cos(olat) * math.sin(dlon/2.0) ** 2.0
+    c = 2.0 * math.asin(min(1.0,math.sqrt(a)))
+    d = R * c
+  except Exception, exc:
+    print "MATH ERROR:", lat1, lon1, dlon, dlat, a, exc
+    return 0.0
+  return d
+  
+
+distance = earthDistance
+
+class Waypoint(list):
+  FIELDS = [ "name", "lat", "lon", "alt", "time",
+             "speed","hdg", "fix", "hdop","vdop", "pdop",
+             "hacc", "vacc" ]
+  
+  def __idx(self, name):
+    if not name in self.FIELDS:
+      raise AttributeError, name
+    return self.FIELDS.index(name)
+
+  def __getattr__(self, name):
+    return self[self.__idx(name)]
+
+  def __setattr__(self, name, val):
+    self[self.__idx(name)] = val
+
+class GpsLogfile(object):
+  def __init__(self, logname=None, logdir=DEF_LOGDIR, ext=".log"):
+    self.ts = time.time()
+    if logname == None:
+      logname = time.strftime("GpsLog-%Y%m%d-%H%M%S", time.gmtime(self.ts))
+    logname = os.path.join(logdir, logname)
+      
+    if ext and not logname.endswith(ext):
+      logname += ext
+
+    self.logname  = logname
+    self.numtrk   = 0
+    self.dist     = 0.0
+    self.prev     = None
+    self.waypts   = []
+    
+  def format(self, gps, skipped=0):
+    self.numtrk   += 1
+    if self.prev:
+      self.dist   += distance(gps.position, self.prev)
+    self.prev      = gps.position
+    
+  def waypoint(self, gps, name=None):
+    if name == None: name = "Waypoint %d" % (len(self.waypts)+1)
+    alt = (hasattr(gps, "corralt") and gps.corralt) or gps.alt
+    self.waypts += [ Waypoint((name, gps.lat,  gps.lon,  alt,  gps.time,
+                              gps.speed,gps.hdg,  gps.fix,
+                              gps.hdop, gps.vdop, gps.pdop,
+                              gps.hacc, gps.vacc)) ]
+    return self.waypts[-1]
+
+  def waypoints(self):
+    return self.waypts
+
+  def name(self):
+    return self.logname
+
+  def tracks(self):
+    return self.numtrk
+
+  def distance(self):
+    return self.dist
+
+
+class OziLogfile(GpsLogfile):
+  def __init__(self, logname=None, logdir=DEF_LOGDIR,
+               extended=True, comment="", **_rest):
+    
+    super(OziLogfile, self).__init__(logname=logname, logdir=logdir, ext=".plt")
+
+    if comment: comment = " ("+comment+")"
+    
+    self.file = file(self.name(), "w")
+    self.file.write("""OziExplorer Track Point File Version 2.1
+WGS 84
+Altitude is in Feet
+Reserved 3
+0,2,255,S60 GpsLog%s,0,0,2,8421376
+<numtrk>
+""" % comment)
+    self.extended = extended
+
+  def format(self, gps, skipped=0):
+    super(OziLogfile, self).format(gps, skipped)
+    seg = str(int(skipped != 0))
+    if gps.alt != None: alt = int(gps.corralt * METERS_TO_FEET)
+    else:               alt = -777
+    lat, lon, alt = ("%.6f"%gps.lat, "%.6f"%gps.lon, "%d" % alt)
+    ozitime = ("%13.7f" % ((gps.time / SECONDS_PER_DAY) + DAYS_SINCE_1990)).replace(" ","")
+    if self.extended:
+      ts = gps.gmtime
+      fmttime = time.strftime("%H:%M:%S", ts)
+      fmtdate = time.strftime("%d-%b-", ts)+ time.strftime("%Y",ts)[-2:]
+    else:
+      fmttime = fmtdate = ""
+    self.file.write(",".join([lat,lon,seg,alt,ozitime,fmtdate,fmttime])+"\n")
+    
+  def close(self):
+    self.file.close()
+    
+    if self.tracks() == 0:
+      os.remove(self.name())
+    else:
+      f = file(self.name(), "r")
+      plt = f.read()
+      f.close()
+      plt = plt.replace("<numtrk>", str(self.tracks()))
+      f = file(self.name(), "w")
+      f.write(plt)
+      f.close()
+
+
+class GpxLogfile(GpsLogfile):
+  def __init__(self, logname=None, logdir=DEF_LOGDIR,
+               extended=True, satellites=True, comment="", **_rest):
+
+    super(GpxLogfile, self).__init__(logname=logname, logdir=logdir, ext=".gpx")
+    
+    self.extended   = extended
+    self.satellites = satellites
+
+    self.file = file(self.name(), "w")
+
+    self.gpx  = XMLWriter(self.file, encoding="utf-8")
+      
+    self.gpx.declaration()
+    
+    if comment: comment = " ("+comment+")"
+
+    self.gpx.start("gpx",
+      {"version":   "1.0", "creator": "GpsLog for S60%s" % comment,
+       "xmlns:xsi":          "http://www.w3.org/2001/XMLSchema-instance",
+       "xmlns":              "http://www.topografix.com/GPX/1/0",
+       "xmlns:gpslog":       "http://www.johler.ph/s60gpslog",
+       "xsi:schemaLocation": "http://www.topografix.com/GPX/1/0 http://www.topografix.com/GPX/1/0/gpx.xsd",
+      })
+  
+    self.gpx.start("time")
+    self.gpx.data(isoformat(self.ts))
+    self.gpx.end("time")
+    self.gpx.start("trk")
+    self.gpx.start("trkseg")
+
+  def format(self, gps, skipped=0, name=None):
+    super(GpxLogfile, self).format(gps, skipped)
+    
+    if skipped:
+      self.gpx.end("trkseg")
+      self.gpx.start("trkseg")
+
+    self.gpx.start("trkpt", lat="%.9f"%gps.lat, lon="%.9f"%gps.lon)
+    if gps.alt != None:
+      self.gpx.start("ele");  self.gpx.data("%.6f"%gps.corralt); self.gpx.end()
+    self.gpx.start("time"); self.gpx.data(isoformat(gps.time)); self.gpx.end()
+    if name:
+      self.gpx.start("name"); self.gpx.data(name); self.gpx.end()
+    if skipped:
+      self.gpx.start("cmt"); self.gpx.data("Skipped %d trackpoints!" % skipped); self.gpx.end()
+
+    if self.extended:
+      if gps.sat != None:
+        self.gpx.start("sat");  self.gpx.data(str(gps.nsat)); self.gpx.end()
+      if gps.fix != None:
+        self.gpx.start("fix")
+        self.gpx.data(FIXTYPES[gps.fix-1])
+        self.gpx.end()
+      if gps.hdop != None:
+        self.gpx.start("hdop");  self.gpx.data("%.1f"%gps.hdop); self.gpx.end()
+      if gps.vdop != None:
+        self.gpx.start("vdop");  self.gpx.data("%.1f"%gps.vdop); self.gpx.end()
+      if gps.pdop != None:
+        self.gpx.start("pdop");  self.gpx.data("%.1f"%gps.pdop); self.gpx.end()
+      self.gpx.start("extensions")
+      if gps.speed != None:
+        self.gpx.start("gpslog:speed"); self.gpx.data("%.2f"%gps.speed); self.gpx.end()
+      if gps.hdg != None:
+        self.gpx.start("gpslog:hdg"); self.gpx.data("%.4f"%gps.hdg); self.gpx.end()
+      if gps.hacc != None:
+        self.gpx.start("gpslog:hacc");  self.gpx.data("%.1f"%gps.hacc); self.gpx.end()
+      if gps.vacc != None:
+        self.gpx.start("gpslog:vacc");  self.gpx.data("%.1f"%gps.vacc); self.gpx.end()
+      sat = gps.sat
+      if self.satellites and sat != None:
+        for s in sat:
+          self.gpx.start("gpslog:sat",
+                         prn=str(s.prn), az=str(s.azimuth), ele=str(s.elevation),
+                         signal=str(s.signal), used=(s.used and "true") or "false")
+          self.gpx.end()
+      self.gpx.end("extensions")
+
+    self.gpx.end("trkpt")
+
+  def waypoint(self, gps, name=None):
+    wpt = super(GpxLogfile, self).waypoint(gps, name)
+    self.format(gps, name=wpt[0])
+    return wpt
+
+  def close(self):
+    if self.gpx != None:
+      self.gpx.close(0);
+      self.gpx.comment("Summary:")
+      self.gpx.comment("Trackpoints: %d" % self.tracks())
+      self.gpx.comment("Distance:    %.1fkm" % (self.distance()/1000.0))
+      del self.gpx; self.gpx = None
+
+    self.file.close()
+    if self.tracks() == 0:
+      os.remove(self.name())
+
+class GpsLogSettings(settings.Settings):
+  def __init__(self, desc_list, fname):
+    self.ext_desc_list = desc_list
+    dl = []; self.hide = {}
+    for (nm, tp, ls, vl) in desc_list:
+      if type(nm) in [tuple, list] and len(nm) > 2:
+        nm, dsp, hide = nm
+        if hide: self.hide[nm] = hide
+        nm = (nm, dsp)
+      dl += [(nm,tp,ls,vl)]
+    settings.Settings.__init__(self, dl, fname)
+
+  def execute_dialog(self, menu=None):
+    saveDl = self.desc_list[:]
+    self.desc_list = [ d for d in self.desc_list if d[0] not in self.hide ]
+    settings.Settings.execute_dialog(self, menu)
+    self.desc_list = saveDl[:]
+
+  def save(self):
+    db = e32dbm.open(self.fname, "c")
+    for (name, type, lst, value) in self.desc_list:
+      db[name] = str(eval("self." + name))
+    db.reorganize()
+    db.close()
+  
+
+  def areNew(self):
+    db = e32dbm.open(self.fname, "c")
+    neew = not db.has_key(self.desc_list[0][0])
+    db.close()
+    return neew
+
+  def reset(self):
+    if os.path.exists(self.fname+".e32dbm"):
+      os.remove(self.fname+".e32dbm")
+    self.__init__(self.ext_desc_list, os.path.basename(self.fname))
+    
---
+++ src/gpssplash.py
@@ -0,0 +1,54 @@
+import appuifw, os, e32, graphics
+
+if os.path.exists(os.path.join(os.path.dirname(__file__), "gpslog.jpg")):
+  IMGFILE = os.path.join(os.path.dirname(__file__), "gpslog.jpg")
+else:
+  IMGFILE = (e32.in_emulator() and r"C:\python\gpslog.jpg") or "gpslog.jpg"
+
+__appbody = appuifw.app.body
+__splash  = None
+__img     = None
+__msg     = None
+
+def __draw(rect=None):
+  if __splash and __img:
+    __splash.clear(0)
+    w, h   = __splash.size
+    wi, hi = __img.size
+    x, y = (w/2-wi/2, h/2-hi/2-15)    # sic!
+    __splash.blit(__img, target=(x, y))
+    if __msg:
+      msr = __splash.measure_text(__msg, font="normal")[0]
+      dx, dy = msr[2], msr[3]-msr[1]
+      __splash.text((w/2-dx/2, h-dy-2), __msg, fill=0xffffff, font="normal")
+
+def show(msg="Loading..."):
+  # appuifw.app.body.set(u"Loading...")
+  global __splash, __img, __msg
+  __msg = unicode(msg)
+  if not __splash:
+    __splash = appuifw.Canvas(__draw)
+    r = min(__splash.size[0],__splash.size[1])*2/3
+    try:    img = graphics.Image.open(IMGFILE)
+    except: img = graphics.Image.new((r,r)); img.clear(0)
+    __img    = img.resize((r,r), keepaspect=True)
+    appuifw.app.body = __splash
+    del img
+  __draw()
+  e32.ao_yield()
+  # e32.ao_yield()
+  
+
+def hide():
+  # appuifw.app.body.set(u"")
+  global __splash, __img
+  if __img:
+    del __img
+    __img = None
+    
+  if __splash:
+    appuifw.app.body = __appbody
+    del __splash
+    __splash = None
+    
+  e32.ao_yield()
