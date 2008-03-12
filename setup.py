90c8345675c8b72bc31d0227c0ec76c79a5b1d59
johler <johler@ac97095f-ae47-0410-9732-21da4afb212a>
2008-03-12 13:03:50 +0000

    Project Administration

--- setup.py
+++ setup.py
@@ -13,31 +13,37 @@ scaps   = caps + "+Location+SwEvent+ReadDeviceData+WriteDeviceData"
 
 SYMBIAN_UID=0xEDED0AF2
 
-references = [
+copy = [
+  ("../../GPS/gpslib/*.py", "src/gpslib"),
+  ("../Doc/pygpslog/README.wiki", "doc/README"),
+]
+references = [ # a few reminders
   (r"Z:\epoc32\winscw\c\python\gpslog\gpslog.py", r"src\gpslog.py"),
   (r"Z:\epoc32\winscw\c\python\gpslog\gpslib\gpssim.py", r"E:\Develop\Python\Proj\GPS\gpslib\gpssim.py"),
   (r"Z:\epoc32\winscw\c\python\gpslog\gpslib\gpsloc.py", r"E:\Develop\Python\Proj\GPS\gpslib\gpsloc.py"),
   (r"Z:\epoc32\winscw\c\python\gpslog\gpslib\gpsnmea.py", r"E:\Develop\Python\Proj\GPS\gpslib\gpsnmea.py"),
   (r"Z:\epoc32\winscw\c\python\gpslog\gpslib\gpspos.py", r"E:\Develop\Python\Proj\GPS\gpslib\gpspos.py"),
+  (r"Z:\epoc32\winscw\c\python\gpslog\gpslib\gpsstream.py", r"E:\Develop\Python\Proj\GPS\gpslib\gpsstream.py"),
+  (r"Z:\epoc32\winscw\c\python\gpslog\gpslib\gpsbase.py", r"E:\Develop\Python\Proj\GPS\gpslib\gpsbase.py"),
 ]
-  
+unneeded  =  [ "gpslib/SIRF.py", "gpslib/gpssirf.py", "gpslib/gpssim.py", "gpslib/xmldata.py" ]
+
 setup_opts = {
   "caps":    caps,
   "scaps":   scaps,                     # signed caps
   "uid":     SYMBIAN_UID,               # open uid
   "suid":    findUid(appname, srcdir),  # signed uid
   "version": findVersion(appname, srcdir),
-  "copy":  [ ("../../GPS/gpslib/*.py", "src/gpslib"),
-             ("../Doc/pygpslog/README.wiki", "doc/README"),
-  ],
-  "text":  "src/disclaimer.txt",
-  "cert":  sys.argv[1], "key":   sys.argv[2], "pwd":   sys.argv[3],
-  "icon":  "icons\%s.svg" % appname,
-  "src" :  None, # don't build sis with python sources
+  "copy":    copy,
+  "text":    "src/disclaimer.txt",
+  "cert":    sys.argv[1], "key":   sys.argv[2], "pwd":   sys.argv[3],
+  "icon":    "icons\%s.svg" % appname,
+  "src" :    None, # don't build sis with python sources
+  "ignore":  unneeded,
 # "bin":  None,
-  "drive": "C",  # sorry!
-
+# "drive": "C",  # sorry!
 # "keep":  True,
+# "slf": None, "uns": None,
 }
 merges = [ r"E:\Develop\Python\Proj\S60\Carbide\modules\LocationRequestor\sis\LocationReq_3rd.sisx",
            r"E:\Develop\Python\Proj\S60\Carbide\modules\Landmarks\sis\Landmarks_3rd.sisx" ]
